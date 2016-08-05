fb784a60ca0f9efb00ecbbec7102ad6c1f5a50a0
Ádám Gólya <adam.stork@gmail.com>
2016-08-05 17:12:55 +0200

    feat(index.js): add directory register function
    From now we can register our services from a directory.
    
    Signed-off-by: Ádám Gólya <adam.stork@gmail.com>

--- features/steps/di.js
+++ features/steps/di.js
@@ -1,13 +1,23 @@
 'use strict';
 
+const expect = require('chai').expect;
+const fs = require('fs');
+
 module.exports = function() {
   this.Given('a directory with a service "$serviceName"', function(serviceName, callback) {
-    callback();
+    fs.access('features/assets/test/index.js', function (exists) {
+      if (!exists) {
+        callback();
+        return;
+      }
+      callback('The given service does not exists');
+    })
   });
   this.When('I start the application', function(callback) {
     callback();
   });
   this.Then('I am able to use the "$serviceName" dependency in my internal service', function(serviceName, callback) {
+    expect(this.container.get('test').test()).to.eql('test');
     callback();
   });
 
--- index.js
+++ index.js
@@ -1,4 +1,33 @@
 'use strict';
 
-const di = require('bottlejs');
+const di = new require('bottlejs')();
+const fs = require('fs');
+const _ = require('lodash');
 
+module.exports = function () {
+
+  function registerDir(path) {
+    fs.readdir(path, function(err, serviceDirectories) {
+      if (err) {
+        throw new Error(`Can not register directory: ${path}`);
+      }
+
+      _.forEach(serviceDirectories, function (serviceDirectory){
+        const servicePath = [path, serviceDirectory].join('');
+        const module = require(servicePath);
+        di.service(serviceDirectory, module);
+        const argList = [serviceDirectory, module].concat(module.deps || [])
+        di.service.apply(di, argList);
+      });
+    });
+  }
+
+  function get(dependencyName) {
+    return di.container[dependencyName];
+  }
+
+  return Object.freeze({
+    registerDir,
+    get: get
+  });
+}
