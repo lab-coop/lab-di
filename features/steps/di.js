88302419c4f446b9810d3acd8749bc162d00c834
Ádám Gólya <adam.stork@gmail.com>
2016-11-09 17:19:12 +0100

    chore(steps/di.js): clean up

--- features/steps/di.js
+++ features/steps/di.js
@@ -5,7 +5,6 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = function() {
-  // Scenario: I see the parsed dependencies from a directory
   this.Given('a directory with a lab- prefixed service "$serviceName"',
     function(serviceName, callback) {
       fs.access('assets/'+ serviceName+ '/index.js', function (nonexistent) {
@@ -26,12 +25,12 @@ module.exports = function() {
     this.container = di;
     callback();
   });
+
   this.Then('I am able to use the "$serviceName" dependency in my internal service', function(serviceName, callback) {
     expect(this.container.get(serviceName).test()).to.eql('test');
     callback();
   });
 
-  // Scenario: I can use different implementations of a registered service
   this.Given('an implemenation "$implementationName" in service "$serviceName"', function(implementationName, serviceName, callback) {
     fs.access(`assets/${serviceName}/implementations/${implementationName}.js`, function (nonexistent) {
       if (!nonexistent) {
@@ -41,15 +40,12 @@ module.exports = function() {
       callback('The given implementation does not exist');
     })
   });
-  this.When('I use service "$serviceName" with "$implementationName"', function(serviceName, implementationName, callback) {
-    callback();
-  });
+
   this.Then('I see result "$result" for service "$serviceName" with implementation "$implementationName"', function(result, serviceName, implementationName, callback) {
     expect(this.container.get(`${serviceName}-${implementationName}`).test()).to.eql(result);
     callback();
   });
-  
-  // Scenario: I can not register a non-lab- prefixed module
+
   this.Given('a directory without a lab- prefix "$moduleName" exists', function(moduleName, callback) {
     fs.access('assets/'+ moduleName + '/index.js', function (nonexistent) {
       if(nonexistent) {
@@ -59,6 +55,7 @@ module.exports = function() {
       callback();
     });
   });
+
   this.Then('I see the module "$moduleName" is undefined in the di', function(moduleName, callback) {
     expect(this.container.get(moduleName)).to.not.exist;
     callback();
